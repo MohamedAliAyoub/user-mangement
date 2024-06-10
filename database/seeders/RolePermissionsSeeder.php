3dfd921799aae2fe0cbd8272b28414d20acb1872
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-06-10 12:57:31 +0200

    [FEAT] add roles and permissions in seeder

---
+++ database/seeders/RolePermissionsSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolePermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['manager', 'employee', 'director', 'client'];
+        $permissions = ['view_users', 'edit_users', 'delete_users', 'create_users'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // Assign permissions to roles
+        $rolePermissions = [
+            'manager' => ['view_users', 'edit_users', 'create_users'],
+            'employee' => ['view_users'],
+            'director' => ['view_users', 'edit_users', 'delete_users', 'create_users'],
+            'client' => ['view_users'],
+        ];
+    }
+}
